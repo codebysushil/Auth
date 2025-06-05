ac7b4657f560c07c97ad2269a3679be5b97cb100
Sushil Kumar <sushilkumbhar77@gmail.com>
2025-06-05 17:12:22 +0530

    Update

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
 
 require_once __DIR__."/vendor/autoload.php";
-require_once("./public/index.html");
+//require_once("./public/index.html");
+
+session_start();
+
+if(!isset($_SESSION['user_id'])){
+  print("<h2>Login please.</h2>");
+  echo '<a href="/login">Login?</a>';
+}
