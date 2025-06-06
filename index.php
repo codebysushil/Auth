aa4c03aeca512eaafbc76529fae5eba371a93687
Sushil Kumar <sushilkumbhar77@gmail.com>
2025-06-06 14:03:28 +0530

    update

--- index.php
+++ index.php
@@ -6,6 +6,11 @@ require_once __DIR__."/vendor/autoload.php";
 session_start();
 
 if(!isset($_SESSION['user_id'])){
+  header('Location: ./public/login.html');
+  die();
   print("<h2>Login please.</h2>");
   echo '<a href="/login">Login?</a>';
+} else {
+  header('Location: ./public/index.html');
+  exit();
 }
