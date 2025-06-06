b3a25ba3376b70957539ee20c7e9717e7d4b0072
Sushil Kumar <sushilkumbhar77@gmail.com>
2025-06-06 13:25:27 +0530

    update

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+const form = document.querySelector('form');
+const name = document.querySelector('#name');
+const email = document.querySelector('#email');
+const pass = document.querySelector('#password');
+const cpass = document.querySelector('#cpassword');
+
+
+
