c474c80718c2e417ec01a014600c912c927a733e
Sushil Kumar <sushilkumbhar77@gmail.com>
2025-06-10 17:17:21 +0530

    Jquery AJAX

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,28 @@
 $(document).ready(function () {
-    $("#login").on("click", function (e) {
-        e.preventDefault();
+    $("#login").on("submit", function () {
+        //e.preventDefault();
         const email = $("#email").val();
         const password = $("#password").val();
-        
-        $.post("../src/login.php", {
-            user_email: email,
-            user_password: password
-        }, function(data, status){
-            alert("Data: " + data + "\nStatus: " + status);
-        });
+
+        $.post(
+            "../src/login.php",
+            {
+                user_email: email,
+                user_password: password
+            },
+            function (data, status) {
+                //alert("Message: " + data);
+                if (data == "Invalid email format.") {
+                    const msg = $("#mailErr").html("Invalid email format.");
+                } else if (data == "Please provide your email.") {
+                    const msg = $("#mailErr").html(
+                        "Please provide your email."
+                    );
+                }
+
+                const errPass = $("#passErr").html(data);
+            }
+        );
         //console.log(email, password);
     });
 });
