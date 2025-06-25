f3f04d3298623f93fa8760768837b49f2fa19ee0
Sushil Kumar <sushilkumbhar77@gmail.com>
2025-06-25 08:30:57 +0530

    class autoloading

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once(__DIR__."/vendor/autoload.php");
+
+spl_autoload_register(function ($class) {
+    $file = __DIR__ . DIRECTORY_SEPARATOR . str_replace("\\", DIRECTORY_SEPARATOR, $class) . '.php';
+
+    if (file_exists($file)) {
+        require_once $file;
+    } else {
+        throw new Exception("This page not found: $file");
+
+    }
+});
+//$app->run();
