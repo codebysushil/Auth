4c8eabeef0a78a354ab16103c2e4a6930ad49976
Sushil Kumar <sushilkumbhar77@gmail.com>
2025-06-27 14:03:48 +0530

    fresh start

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Auth Project, PHP Project">
+    <meta name="robots" content="index, follow">
+    <link rel="icon" type="image/png" href="/icon.png">
+    <title><?= $title ?? 'Auth Project';?></title>
+
+    <link rel="stylesheet" href="./css/app.css" />
+</head>
+<body>
+
+
+<script src="./js/app.js"></script>
+</body>
+</html>
