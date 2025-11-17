49e5df618cbb47b0aa964dbe9c431f01d2270c07
Sushil Kumar <sushilkumbhar77@gmail.com>
2025-11-17 14:53:51 +0530

    oop

--- public/index.php
+++ public/index.php
@@ -8,11 +8,14 @@
     <meta name="robots" content="index, follow">
     <link rel="icon" type="image/png" href="http://localhost:8080/public/images/logo.png">
     <title><?= $title ?? 'Auth Project'; ?></title>
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/app.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 </head>
 <body>
-<img width="100%" src="http://localhost:8080/public/images/logo.png">
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 <script src="./js/app.js"></script>
 </body>
 </html>
