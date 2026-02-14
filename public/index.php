f768f1f19149b4e53d85eda61dba7a7246b436fb
Sushil Kumar <sushilkumbhar77@gmail.com>
2026-02-14 20:03:00 +0530

    updated

--- public/index.php
+++ public/index.php
@@ -1,16 +1,16 @@
 <?php
 
-//define('APP_ROOT', __DIR__);
+// define('APP_ROOT', __DIR__);
 
-require_once(__DIR__."/../vendor/autoload.php");
-//'require_once(__DIR__."/../bootstrap/");
+require_once __DIR__.'/../vendor/autoload.php';
+// 'require_once(__DIR__."/../bootstrap/");
 
-spl_autoload_register(function($class) {
-    $classFile = str_replace("\\", DIRECTORY_SEPARATOR, $class.".php");
-    $classPath = "./../app/" . $classFile;
+spl_autoload_register(function ($class) {
+    $classFile = str_replace('\\', DIRECTORY_SEPARATOR, $class.'.php');
+    $classPath = './../app/'.$classFile;
 
-    if(file_exists($classPath)){
-        require_once($classPath);
+    if (file_exists($classPath)) {
+        require_once $classPath;
     }
 });
 
